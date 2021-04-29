a33d85bebde6b52b4b7b7278b71a151e5b30cc58
sehob2020 <70333786+sehob2020@users.noreply.github.com>
2021-04-29 12:24:00 +0200

    brightness added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.listen(3000, () => {
 
 // GET AC
 app.get('/api/devices/ac/ac1', (req, res) => {
-    // Allowing us to use querys in our request handling
+    // Allowing us to turn devices on/off in our query
     const power = req.query.power === 'on' ? true : false;
 
     // Using the diffrent functions included in the assignment
@@ -31,7 +31,7 @@ app.get('/api/devices/ac/ac1', (req, res) => {
 
 // GET BLIND
 app.get('/api/devices/blinds/bli1', (req, res) => {
-    // Allowing us to use querys in our request handling
+    // Allowing us to turn devices on/off in our query
     const power = req.query.power === 'on' ? true : false;
 
     // Using the diffrent functions included in the assignment
@@ -50,11 +50,14 @@ app.get('/api/devices/blinds/bli1', (req, res) => {
 
 // GET LIGHT 1
 app.get('/api/devices/lights/lig1', (req, res) => {
-    // Allowing us to use querys in our request handling
+    // Allowing us to turn devices on/off in our query
     const power = req.query.power === 'on' ? true : false;
 
+    // Allowing us to adjust the brightness in our query
+    const brightness = req.query.brightness;
+
     // Using the diffrent functions included in the assignment
-    const result = db.get('devices').find({ id : 'LIG1' }).assign({ on : power }).value()
+    const result = db.get('devices').find({ id : 'LIG1' }).assign({ on : power, brightness : brightness }).value()
 
     if(result){
         res.send(result)
@@ -69,11 +72,14 @@ app.get('/api/devices/lights/lig1', (req, res) => {
 
 // GET LIGHT 2
 app.get('/api/devices/lights/lig2', (req, res) => {
-    // Allowing us to use querys in our request handling
+    // Allowing us to turn devices on/off in our query
     const power = req.query.power === 'on' ? true : false;
 
+    // Allowing us to adjust the brightness in our query
+    const brightness = req.query.brightness;
+
     // Using the diffrent functions included in the assignment
-    const result = db.get('devices').find({ id : 'LIG2' }).assign({ on : power }).value()
+    const result = db.get('devices').find({ id : 'LIG2' }).assign({ on : power, brightness : brightness }).value()
 
     if(result){
         res.send(result)
@@ -88,11 +94,14 @@ app.get('/api/devices/lights/lig2', (req, res) => {
 
 // GET LIGHT 3
 app.get('/api/devices/lights/lig3', (req, res) => {
-    // Allowing us to use querys in our request handling
+    // Allowing us to turn devices on/off in our query
     const power = req.query.power === 'on' ? true : false;
 
+    // Allowing us to adjust the brightness in our query
+    const brightness = req.query.brightness;
+
     // Using the diffrent functions included in the assignment
-    const result = db.get('devices').find({ id : 'LIG3' }).assign({ on : power }).value()
+    const result = db.get('devices').find({ id : 'LIG3' }).assign({ on : power, brightness : brightness }).value()
 
     if(result){
         res.send(result)
@@ -107,7 +116,7 @@ app.get('/api/devices/lights/lig3', (req, res) => {
 
 // GET CAMERA
 app.get('/api/devices/cameras/cam1', (req, res) => {
-    // Allowing us to use querys in our request handling
+    // Allowing us to turn devices on/off in our query
     const power = req.query.power === 'on' ? true : false;
 
     // Using the diffrent functions included in the assignment
@@ -126,7 +135,7 @@ app.get('/api/devices/cameras/cam1', (req, res) => {
 
 // GET LOCK
 app.get('/api/devices/locks/loc1', (req, res) => {
-    // Allowing us to use querys in our request handling
+    // Allowing us to turn devices on/off in our query
     const power = req.query.power === 'on' ? true : false;
 
     // Using the diffrent functions included in the assignment
@@ -145,7 +154,7 @@ app.get('/api/devices/locks/loc1', (req, res) => {
 
 // GET VACUUM
 app.get('/api/devices/vacuums/vac1', (req, res) => {
-    // Allowing us to use querys in our request handling
+    // Allowing us to turn devices on/off in our query
     const power = req.query.power === 'on' ? true : false;
 
     // Using the diffrent functions included in the assignment
@@ -164,7 +173,7 @@ app.get('/api/devices/vacuums/vac1', (req, res) => {
 
 // GET SPEAKER
 app.get('/api/devices/speakers/spe1', (req, res) => {
-    // Allowing us to use querys in our request handling
+    // Allowing us to turn devices on/off in our query
     const power = req.query.power === 'on' ? true : false;
 
     // Using the diffrent functions included in the assignment
